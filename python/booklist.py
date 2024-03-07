682c13bce2f2f7830c8311199c0dfb0c7a6c39b6
weslykim <kimjinse@naver.com>
2024-03-07 16:57:54 +0900

    ss

--- python/booklist.py
+++ python/booklist.py
@@ -2,8 +2,32 @@ import pymysql
 
 
 def main():
-        print("hello, world!")
+        host = "localhost"
+        port = 3306
+        database = "madangdb"
+        username = "root"
+        password = "0000"
+
+        config = True
+        conn = pymysql.connect(host=host, port=port, db=database, user=username
+        , password=password, charsset="utf8")
+        print("연결 성공")
+        except: # ignore error
+            print("연결 실패")
+            config = False
+
+        if config:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM Book")
+            books = cursor.fetchall()
+            for book in books:
+                    print(book)
+                    cursor.close()
+                    conn.close()
+
+if  name    ==  "main"                    
+                
 
 
-if  name    == "    main    ";
-    main()
+
+    
