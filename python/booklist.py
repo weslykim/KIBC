4155284c7452b57419c0ca4e958d05b3e265019e
weslykim <kimjinse@naver.com>
2024-03-07 17:12:27 +0900

    xx

--- python/booklist.py
+++ python/booklist.py
@@ -2,31 +2,31 @@ import pymysql
 
 
 def main():
-        host = "localhost"
-        port = 3306
-        database = "madangdb"
-        username = "root"
-        password = "0000"
+    host = "localhost"
+    port = 3306
+    database = "madangdb"
+    username = "root"
+    password = "0000"
 
-        config = True
-        try:
-            conn = pymysql.connect(host=host, port=port, db=database, user=username
-            , password=password, charsset="utf8")
-            print("연결 성공")
-        except: # ignore error
-            print("연결 실패")
-            config = False
+    config = True
+    try:
+        conn = pymysql.connect(host=host, port=port, db=database, user=username
+        , password=password, charset="utf8")
+        print("연결 성공")
+    except: # ignore error
+        print("연결 실패")
+        config = False
 
-        if config:
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM Book")
-            books = cursor.fetchall()
-            for book in books:
-                    print(book)
-                    cursor.close()
-                    conn.close()
-                    if name == "main"
-                    main()   
+    if config:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Book")
+        books = cursor.fetchall()
+        for book in books:
+            print(book)
+        cursor.close()
+        conn.close()
+if __name__ == "__main__":
+    main()   
                   
                 
 
