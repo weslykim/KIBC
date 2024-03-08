f03968fcea13bcbe78b5ff04abe40b9bb1d56d4e
weslykim <kimjinse@naver.com>
2024-03-08 10:25:55 +0900

    ss

---
+++ python_C_mysql/build/CMakeFiles/booklist.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubnt/kuIotBigdataClass/python_C_mysql/booklist.c" "CMakeFiles/booklist.dir/booklist.c.o" "gcc" "CMakeFiles/booklist.dir/booklist.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
