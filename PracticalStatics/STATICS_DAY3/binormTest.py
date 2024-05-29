f280345b835225d7787f3e0d0742da690a84d0b0
weslykim <kimjinse1234@naver.com>
2024-05-29 15:58:23 +0900

    plus

--- PracticalStatics/STATICS_DAY3/binormTest.py
+++ PracticalStatics/STATICS_DAY3/binormTest.py
@@ -7,6 +7,8 @@ def main():
 
     print(stats.poisson.rvs(2, size = 100))
     print(stats.poisson.rvs(2, size = 100).mean())
+    print(stats.expon.rvs(scale = 1/0.2, size = 100).mean())
+    print(stats.expon.rvs(scale = 5, size = 100).mean())
 
 if __name__ == "__main__":
     main()
