736ede19a92a7a1354b42d7baed184a9e6a02aa1
weslykim <kimjinse1234@naver.com>
2024-05-29 14:51:11 +0900

    dataplus

--- PracticalStatics/STATICS_DAY3/resampleTest.py
+++ PracticalStatics/STATICS_DAY3/resampleTest.py
@@ -1,11 +1,18 @@
 import sklearn.utils as resample
 import numpy as np
+import pandas as pd
 def main():
+    folder = "/home/ubnt/KIBC/PracticalStatics/data/"
+    loans_income : pd.DataFrame = pd.read_csv(folder + "loans_income.csv").squeeze('columns')
     results = []
     for nrepeat in range(1000):
-        sample = np.array(resample.resample(range(10), n_samples=5))
-        results.append(np.median(sample))
-
+        sample = resample.resample(loans_income)
+        results.append(sample.median())
+    results = pd.Series(results)
+    print("부트스트랩")
+    print(f"원본 : {loans_income.median()}")
+    print(f"바이어스 : {results.mean() - loans_income.median()}")
+    print(f"표준편차 : {results.std()}")
 
 if __name__ == "__main__":
     main()
