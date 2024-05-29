7ba71c0c04196d8a272bbcbc09d45436a59c78bc
weslykim <kimjinse1234@naver.com>
2024-05-29 16:31:10 +0900

    graphpluscode

--- PracticalStatics/STATICS_DAY3/binormTest.py
+++ PracticalStatics/STATICS_DAY3/binormTest.py
@@ -1,15 +1,29 @@
 import scipy.stats as stats
 import pandas as pd
-
+import matplotlib.pyplot as plt
 def main():
+    # 이산분포
     print(stats.binom.pmf(2, n = 5, p = 0.1))
     print(stats.binom.cdf(2, n = 5, p = 0.1))
+    # binom_list = []
+    # for _ in range(1000):
+    #     a = print(pd.Series(stats.binom.rvs(2, p = 0.1, size = 100)))
+    #     binom_list.append(a[a == 1].count())
+    # pd.Series(binom_list).plot.hist()
+    # plt.show()
+    
+    # sample = stats.poisson.rvs(2, size = 100)
+    # pd.Series(sample).plot.hist()
+    # plt.show()
+    
+    # sample = stats.expon.rvs(scale = 1/0.2, size = 100)
+    # pd.Series(sample).plot.hist()
+    # plt.show()
 
-    print(stats.poisson.rvs(2, size = 100))
-    print(stats.poisson.rvs(2, size = 100).mean())
-    print(stats.expon.rvs(scale = 1/0.2, size = 100).mean())
-    print(stats.expon.rvs(scale = 5, size = 100).mean())
-    print(stats.weibull_min.rvs(c = 1.5, scale = 5000, size = 100))
+    sample = stats.weibull_min.rvs(c = 1.5, scale = 5000, size = 100)
+    pd.Series(sample).plot.hist()
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
