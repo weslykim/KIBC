3b9c766d8ff9e0a00140920e5c50c1aa5ac19776
weslykim <kimjinse1234@naver.com>
2024-05-29 10:14:42 +0900

    ss

--- PracticalStatics/STATICS_DAY1/correlation.py
+++ PracticalStatics/STATICS_DAY1/correlation.py
@@ -32,7 +32,7 @@ def main():
     folder = "/home/ubnt/KIBC/PracticalStatics/data/"
     sp500_px = pd.read_csv(folder + "sp500_data.csv", index_col=0)
     sp500_sym = pd.read_csv(folder + "sp500_sectors.csv")
-    # etfs = sp500_px.loc[sp500_px.index > '2012-07-01', sp500_sym[sp500_sym['sector'] == 'etf']['symbol']]
+    etfs = sp500_px.loc[sp500_px.index > '2012-07-01', sp500_sym[sp500_sym['sector'] == 'etf']['symbol']]
     # sns.heatmap(etfs.corr(), vmin =-1, vmax =1, cmap =sns.diverging_palette(20, 220, as_cmap =True))
     fig, ax = plt.subplot(figsize = (5, 4))
     m, ax = plot_corr_ellipses(etfs.corr(), figsize = (5, 4), cmap = 'bar_r')
