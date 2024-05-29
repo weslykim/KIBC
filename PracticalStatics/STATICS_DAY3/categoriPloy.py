4abd976dc48dd2e6f749380658693d23919d5799
weslykim <kimjinse1234@naver.com>
2024-05-29 12:44:49 +0900

    plus

--- PracticalStatics/STATICS_DAY3/categoriPloy.py
+++ PracticalStatics/STATICS_DAY3/categoriPloy.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+def hexbin(x, y, color, **kwargs):
+    cmap = sns.light_palette(color, as_cmap=True)
+    plt.hexbin(x, y, gridsize = 25, cmap = cmap, **kwargs)
 def main():
     folder = "/home/ubnt/KIBC/PracticalStatics/data/"
     lc_loans = pd.read_csv(folder + "lc_loans.csv")
@@ -16,16 +20,29 @@ def main():
     print(perc_crosstab)
 
     # # air_line boxplot 번주형 vs 수치형
-    airline_stats = pd.read_csv(folder + "airline_stats.csv")
+    # airline_stats = pd.read_csv(folder + "airline_stats.csv")
     # ax = airline_stats.boxplot(by = 'airline', column = 'pct_carrier_delay')
     # ax.set_xlabel('')
     # ax.set_ylabel('Daily % of Delayed Flights')
     # plt.suptitle('')
     # plt.show()
-    print(airline_stats.info())
-    ax = sns.violinplot(data = airline_stats, x='airline', y='pct_carrier_delay', inner='quartile', color='white')
-    ax.set_xlabel('')
-    ax.set_ylabel('Daily % of Delayed Flights')
+    # print(airline_stats.info())
+    # ax = sns.violinplot(data = airline_stats, x='airline', y='pct_carrier_delay', inner='quartile', color='white')
+    # ax.set_xlabel('')
+    # ax.set_ylabel('Daily % of Delayed Flights')
+    # plt.show()
+
+    # zipcode tex
+    zip_codes = [98188, 98105, 98108, 98126]
+    kc_tax = pd.read_csv(folder + "kc_tax.csv.gz")
+    kc_tax_zip = kc_tax.loc[kc_tax.ZipCode.isin(zip_codes), :]
+    print(kc_tax_zip.info())
+
+    g = sns.FacetGrid(kc_tax_zip, col = 'ZipCode', col_wrap = 2)
+    g.map(hexbin, 'SqFtTotLiving', 'TaxAssessedValue', extent = [0, 3500, 0, 700000])
+    g.set_axis_labels('Finished Square Feet', 'Tax Assessed Value')
+    g.set_titles('Zip code {col_name:.0f}')
+    plt.colorbar()
     plt.show()
 if __name__ == "__main__":
     main()
