d2619cac97d7399d2b467116db4cfe686790f841
weslykim <kimjinse1234@naver.com>
2024-05-29 10:21:08 +0900

    codechange

--- PracticalStatics/STATICS_DAY1/correlation.py
+++ PracticalStatics/STATICS_DAY1/correlation.py
@@ -4,6 +4,7 @@ from statsmodels import robust
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.collections import EllipseCollection
+from matplotlib.colors import Normalize
 
 def plot_corr_ellipses(data, figsize = None, **kwargs):
     M = np.array(data)
@@ -11,7 +12,7 @@ def plot_corr_ellipses(data, figsize = None, **kwargs):
         raise ValueError('data must be a 2D array')
     fig, ax = plt.subplots(1, 1, figsize=figsize, subplot_kw = {'aspect': 'equal'})
     ax.set_xlim(-0.5, M.shape[1] -0.5)
-    ax.set_ylim(M.shape[0], -0.5, -0.5)
+    ax.set_ylim(M.shape[0] -0.5, -0.5)
     ax.invert_yaxis()
 
     xy = np.indices(M.shape)[::-1].reshape(2, -1).T
@@ -20,7 +21,7 @@ def plot_corr_ellipses(data, figsize = None, **kwargs):
     h = 1 - np.abs(M).ravel() - 0.01
     a = 45 * np.sign(M).ravel()
 
-    ec = EllipseCollection(width = w, height = h, angles = a, units = 'x', offsets = xy, 
+    ec = EllipseCollection(widths = w, heights = h, angles = a, units = 'x', offsets = xy, 
                            norm = Normalize(vmin = -1, vmax = 1), transOffset = ax.transData, array=M.ravel(),**kwargs)
     ax.add_collection(ec)
     if isinstance(data, pd.DataFrame):
@@ -28,15 +29,17 @@ def plot_corr_ellipses(data, figsize = None, **kwargs):
         ax.set_xticklabels(data.columns, rotation = 90)
         ax.set_yticks(np.arange(M.shape[0]))
         ax.set_yticklabels(data.index)
+
+        return ec, ax
 def main():
     folder = "/home/ubnt/KIBC/PracticalStatics/data/"
     sp500_px = pd.read_csv(folder + "sp500_data.csv", index_col=0)
     sp500_sym = pd.read_csv(folder + "sp500_sectors.csv")
     etfs = sp500_px.loc[sp500_px.index > '2012-07-01', sp500_sym[sp500_sym['sector'] == 'etf']['symbol']]
     # sns.heatmap(etfs.corr(), vmin =-1, vmax =1, cmap =sns.diverging_palette(20, 220, as_cmap =True))
-    fig, ax = plt.subplot(figsize = (5, 4))
-    m, ax = plot_corr_ellipses(etfs.corr(), figsize = (5, 4), cmap = 'bar_r')
-    cb = fig.colorbar(m, ax=ax)
+    # fig, ax = plt.subplot(figsize = (5, 4))
+    m, ax = plot_corr_ellipses(etfs.corr(), figsize = (5, 4), cmap = 'bwr_r')
+    cb = plt.colorbar(m, ax=ax)
     cb.set_label('Correlation coefficient')
     plt.tight_layout()
     plt.show()
