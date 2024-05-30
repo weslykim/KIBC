778140b1b15c89e9d992c456e9845ae5faf73a39
weslykim <kimjinse1234@naver.com>
2024-05-30 12:43:58 +0900

    anobaTestplus

---
+++ PracticalStatics/STATICS_DAY4/anobaTest.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def perm_test(df: pd.DataFrame):
+    df = df.copy()
+    df["Time"] = np.random.permutation(df["Time"].values) #type : ignore
+    return df.groupby("Page").mean().values[0]
+def main():
+    folder = "/home/ubnt/KIBC/PracticalStatics/data/"
+    four_session = pd.read_csv(folder + "four_sessions.csv")
+
+    print(four_session.head())
+    print(four_session.info())
+
+    observed_variance = four_session.groupby("Page").std().values.ravel()
+    observed_mean = four_session.groupby("Page").mean().values.ravel()
+    print(f"Observed mean : {observed_mean}")
+    print(f"Observed variance : {observed_variance}")
+
+    perm_variance = [perm_test(four_session) for _ in range(3000)]
+    print(f"Pr(Prob) of permuted variance : {np.mean(perm_variance > observed_variance)}")
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.hist(perm_variance, bins = 11, rwidth = 0.9)
+    ax.axvline(x = observed_variance[0], color = 'black', lw = 2)
+    ax.text(60, 200, 'Observed\nvariance', bbox = {'facecolor': 'white'})
+    ax.set_xlabel("Session time variance")
+    ax.set_ylabel("Frequency")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
