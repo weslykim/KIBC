21aa92f8323ee02ea58b450ef99712b78cbfc3a5
weslykim <kimjinse1234@naver.com>
2024-06-12 11:49:17 +0900

    nesGroupnbdoubleplus

---
+++ machineLearning/newsGroupNB.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import fetch_20newsgroups
+news = fetch_20newsgroups(subset = 'all')
+print(news.keys())
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
