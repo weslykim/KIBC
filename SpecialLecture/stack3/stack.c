42b49f63b72942eabd100f24f9d2d12135607985
weslykim <kimjinse1234@naver.com>
2024-06-18 12:00:44 +0900

    stack3plus

--- SpecialLecture/stack3/stack.c
+++ SpecialLecture/stack3/stack.c
@@ -1,5 +1,6 @@
 #include "stack.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /*struct stack
 {
 	int array[100];
@@ -14,12 +15,22 @@ void push(struct stack *ps, int data)
 {
 	//stack[tos] = data;
 	//++tos;
+	if (ps->tos == ARRAYSIZE)
+	{
+		fprintf(stderr, "Stack is full\n");
+		exit(1);
+	}
 	ps->array[ps->tos] = data;
 	++ps->tos;
 }
 
 int pop(struct stack *ps)
 {
+	if (ps->tos == 0)
+	{
+		fprintf(stderr, "Stack is empty\n");
+		exit(2);
+	}
 	--ps->tos;
 	return ps->array[ps->tos];
 }
