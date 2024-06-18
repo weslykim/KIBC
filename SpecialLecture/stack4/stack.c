3d4e81bd6db7bf532caaab244d55042a6ac339a8
weslykim <kimjinse1234@naver.com>
2024-06-18 12:27:44 +0900

    stack4plus

--- SpecialLecture/stack4/stack.c
+++ SpecialLecture/stack4/stack.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "stack.h"
+#include <assert.h>
 
 void initStack(struct stack *ps, int size)
 {
@@ -17,23 +18,26 @@ void push(struct stack *ps, int data)
 	//stack[tos] = data;
 	//++tos;
 	//if (ps->tos == ARRAYSIZE)
-	if (ps->tos == ps->size)
+	/*if (ps->tos == ps->size)
 	{
 		fprintf(stderr, "Stack is full\n");
 		exit(1);
-	}
+	}*/
+	
+	assert(ps->tos != ps->size);
 	ps->pArr[ps->tos] = data;
 	++ps->tos;
 }
 
 int pop(struct stack *ps)
 {
-	if (ps->tos == 0)
+	/*if (ps->tos == 0)
 	{
 		fprintf(stderr, "Stack is empty\n");
 		exit(2);
-	}
+	}*/
 	--ps->tos;
+	assert(ps->tos != 0);
 	
 	return ps->pArr[ps->tos];
 }
